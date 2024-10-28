a3a183cd51fad18f0d73fa9f2d46f73b96b969df
Saqib Azhar <saqibazhar.07@gmail.com>
2024-10-28 13:21:15 +0500

    Initial Commit

---
+++ src/pages/Admin/index.jsx
@@ -0,0 +1,12 @@
+import Dashboard from "src/pages/Admin/Dashboard/Dashboard.jsx";
+import Main from "src/layouts/Main/Main.jsx";
+
+const Admin = () => {
+    return (
+        <Main>
+            <Dashboard/>
+        </Main>
+    );
+};
+
+export default Admin;
